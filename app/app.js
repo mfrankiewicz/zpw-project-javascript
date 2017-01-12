11cbedb6e5c00798418153adf017797e9b920fd2
Michał Frankiewicz <michal@frankiewicz.me>
2017-01-12 13:47:27 +0100

    dodaje zależności projektu

--- app/app.js
+++ app/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser');
 const app = express();
 const server = app.listen(80);
+const socket = require('socket.io' )(server);
 
-app.get('/', function (req, res) {
-    return res.end('/');
-});
+app.use(express.static(path.join(__dirname, 'html/frontend')));
+app.use('/administrator', express.static(path.join(__dirname, 'html/backend')));
+app.use('/assets', express.static(path.join(__dirname, 'html/assets')));
+
+require('./routes')(app);
