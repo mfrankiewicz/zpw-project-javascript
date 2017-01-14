7becfbbaac7f0f49b1d6a508577c428d7f1bf1ef
Michał Frankiewicz <michal@frankiewicz.me>
2017-01-14 17:48:37 +0100

    prace nad nawigacją

--- app/html/assets/js/scripts.js
+++ app/html/assets/js/scripts.js
@@ -1,4 +1,18 @@
-$(document).on('scroll', function(){
-    $('#browsing-progress').attr('max', $(document).height() - $(window).height());
-    $('#browsing-progress').attr('value', $(window).scrollTop());
+$(document).ready(function() {
+    $("html").niceScroll({
+        cursorcolor: "#e1e1e1",
+        cursorborder: "5px solid #c1c1c1"
+    });
+
+    if ($('#browsing-progress').length) {
+        $(document).on('scroll', function(){
+            $('#browsing-progress').attr('max', $(document).height() - $(window).height());
+            $('#browsing-progress').attr('value', $(window).scrollTop());
+        });
+    }
+
+    $('nav ul li a').on('click', function(){
+        $('nav ul li').removeClass('active');
+        $(this).parent().addClass('active');
+    });
 });
--- app/html/frontend/angular/controllers.js
+++ app/html/frontend/angular/controllers.js
@@ -15,3 +15,43 @@ appControllers.controller('sliderCtrl', function($scope) {
         'http://placehold.it/1920x300/9c29bf/ffffff'
     ];
 });
+
+appControllers.controller('navigationCtrl', function($scope) {
+    pages = [
+        {
+            url: "#!/",
+            label: "Strona główna",
+            active: true
+        },
+        {
+            url: "#!/shop",
+            label: "Strona druga"
+        },
+        {
+            url: "#!/#3",
+            label: "Strona trzecia"
+        },
+        {
+            url: "#!/#4",
+            label: "Strona czwarta"
+        },
+        {
+            url: "#!/#5",
+            label: "Strona piąta"
+        }
+    ];
+
+    pages.reverse();
+
+    pages.every(function(page){
+        if (window.location.href.match(page.url)) {
+            pages[pages.length-1].active = false;
+            page.active = true;
+            return false;
+        }
+
+        return true;
+    });
+    pages.reverse();
+    $scope.pages = pages;
+});
