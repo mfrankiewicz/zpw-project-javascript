b1ad6bd2aa1b6b24688b5b0b71f9271ed3e0e082
Michał Frankiewicz <michal@frankiewicz.me>
2017-01-14 13:02:51 +0100

    organizuje zależności, dodaje sass middleware

--- app/app.js
+++ app/app.js
@@ -3,10 +3,19 @@ const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser');
+const sassMiddleware = require('node-sass-middleware')
 const app = express();
 const server = app.listen(80);
 const socket = require('socket.io' )(server);
 
+
+app.use(sassMiddleware({
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'html/assets/css'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix:  '/assets/css'
+}));
 app.use(express.static(path.join(__dirname, 'html/frontend')));
 app.use('/administrator', express.static(path.join(__dirname, 'html/backend')));
 app.use('/assets', express.static(path.join(__dirname, 'html/assets')));
---
+++ app/html/frontend/angular/app.js
@@ -0,0 +1,6 @@
+var app = angular.module('frontendApp', [
+    'ngRoute',
+    'appControllers',
+    'appServices',
+    'appFilters'
+]);
