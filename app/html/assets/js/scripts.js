0319f76322eeae4e4787e240d71c9c1f17f32470
Michał Frankiewicz <michal@frankiewicz.me>
2017-01-15 17:27:55 +0100

    poprawki stylowania

--- app/html/assets/js/scripts.js
+++ app/html/assets/js/scripts.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     $('html').niceScroll({
         cursorcolor: "#e1e1e1",
-        cursorborder: "5px solid #c1c1c1"
+        cursorborder: "5px solid #545454",
+        zindex: 16
     });
     $('#browsing-progress').progressBar();
     $('nav').navigation();
