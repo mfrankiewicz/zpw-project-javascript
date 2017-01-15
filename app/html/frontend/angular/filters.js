4767d2de10466f19222a6b6962b9702513ce8d24
Michał Frankiewicz <michal@frankiewicz.me>
2017-01-15 19:11:42 +0100

    poprawki paginacji

--- app/html/frontend/angular/filters.js
+++ app/html/frontend/angular/filters.js
@@ -2,6 +2,9 @@ var appFilters = angular.module('appFilters', []);
 
 appFilters.filter('startFrom', function() {
     return function(input, start) {
+        if (!input.length) {
+            return input;
+        }
         start = +start;
         return input.slice(start);
     }
